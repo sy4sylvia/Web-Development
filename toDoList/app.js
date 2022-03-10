fa8cd93e38414bb2ed6f53e55f8201cf86745dc4
Siya Guo <siyaguo@siyas-air.myfiosgateway.com>
2022-03-10 14:34:50 -0500

    ejs update

--- toDoList/app.js
+++ toDoList/app.js
@@ -7,34 +7,14 @@ app.set("view engine", "ejs");
 
 app.get("/", function(req, res) {
   var today = new Date();
-  var curDay = today.getDay();
-  var day = "";
 
-  switch(curDay) {
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    case 0:
-      day = "Sunday";
-      break;
-    default:
-      console.log("Error");
-}
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  var day = today.toLocaleDateString("en-US", options);
 
   res.render("list", {dayType: day}); //key value pair
 
