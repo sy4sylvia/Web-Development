5967086534444a4ad97e725a956b57612238648a
Siya Guo <siyaguo@siyas-air.myfiosgateway.com>
2022-03-11 16:16:26 -0500

    work

--- toDoList/app.js
+++ toDoList/app.js
@@ -6,28 +6,46 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public")); //specify static resource
 
-var items = ["Meal prep", "Data  Structures"];
+let items = ["Meal prep", "Bagel + Salmon for Saturday"];
+let workItems = [];
 
 app.get("/",function(req, res) {
-  var today = new Date();
-  var options = {
+  let today = new Date();
+  let options = {
     weekday: "long",
     day: "numeric",
     month: "long"
   }
-
   var day = today.toLocaleString("en-US", options);
-
   res.render("list", {
-    dayType: day,
+    listTitle: day,
     newListItems: items
   });
-
 });
 
+app.get("/work", function(req, res) { //work route for the work to do list
+  res.render("list", {
+    listTitle: "Work List",
+    newListItems: workItems
+  });
+});
+
+
 app.post("/", function(req, res) {
-  items.push(req.body.newItem);
-  res.redirect("/");
+  if (req.body.list === "Work") { //mainly that after adding an item, doesn't redirect to  home page
+    workItems.push(req.body.newItem);
+    res.redirect("/work");
+  }
+  else {
+    items.push(req.body.newItem);
+    res.redirect("/");
+  }
+});
+
+app.post("/work", function(req, res) {
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
 });
 
 app.listen(3000, function(req, res) {
