d94808f93ec6821f8cf7d2e7528e0ed87e67217c
Siya Guo <siyaguo@siyas-air.myfiosgateway.com>
2022-03-18 09:16:04 -0400

    map filter

--- map-filter-reduce/src/index.js
+++ map-filter-reduce/src/index.js
@@ -1,11 +1,67 @@
+import emojipedia from "./emojipedia";
+
 var numbers = [3, 56, 2, 48, 5];
 
 //Map -Create a new array by doing something with each item in an array.
 
+
+const n = numbers.map(
+    function double(x) {
+        return (x * 2);
+    }
+);
+console.log(n);
+
+
 //Filter - Create a new array by keeping the items that return true.
 
+const newN = numbers.filter(
+    function (num) {
+        return num > 10;
+    }
+
+);
+console.log(newN);
+
+
 //Reduce - Accumulate a value by doing something to each item in an array.
+// var newNumber = 0;
+// numbers.forEach(
+//     function reduce(curN) {
+//         newNumber += curN;
+//     }
+// );
+
+const reduceEg = numbers.reduce ( //sum up the array
+    function (accumulator, curN) {
+        return accumulator + curN;
+    }
+);
+console.log(reduceEg);
 
 //Find - find the first item that matches from an array.
 
+const firstN = numbers.find(
+    function(num) {
+        return num >= 10;
+    }
+);
+console.log(firstN);
+
 //FindIndex - find the index of the first item that matches.
+const firstIdx = numbers.findIndex(
+    function(num) {
+        return num >= 10;
+    }
+);
+console.log(firstIdx);
+
+//a maximus of 100 characters for the meaning of emojis(create an array)
+
+const desc = emojipedia.map(
+    function (emojiEntry) {
+        return (emojiEntry.meaning.substring(0, 100));
+    }
+);
+
+console.log(desc);
