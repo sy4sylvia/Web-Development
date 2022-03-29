b45e1f2afda0220c2b82c6136305293871c78d66
Siya Guo <siyaguo@siyas-air.myfiosgateway.com>
2022-03-29 10:55:51 -0400

    insert using mongoose

--- TryMongoose/app.js
+++ TryMongoose/app.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
-// mongoose.connect('mongodb://localhost:27017/bakeryDB', {useNewUrlParser: true});
-// const bakerySchema = new mongoose.Schema({
-//   name: String,
-//   rating: Number,
-//   review: String
-// });
+mongoose.connect('mongodb://localhost:27017/bakeryDB', {useNewUrlParser: true});
+const bakerySchema = new mongoose.Schema({
+  name: String,
+  rating: Number,
+  review: String
+});
 // const Bakery = mongoose.model("Bakery", bakerySchema);
 // const bakery = new Bakery({
 //   name: "Bagel",
@@ -14,15 +14,38 @@ const mongoose = require('mongoose');
 // });
 // bakery.save();
 
-mongoose.connect('mongodb://localhost:27017/personDB', {useNewUrlParser: true});
-const personSchema = new mongoose.Schema({
-  name: String,
-  age: Number
+// mongoose.connect('mongodb://localhost:27017/personDB', {useNewUrlParser: true});
+// const personSchema = new mongoose.Schema({
+//   name: String,
+//   age: Number
+// });
+//
+// const Person = mongoose.model("Person", personSchema);
+// const person = new Person({
+//   name: "John",
+//   age: 37
+// });
+// person.save();
+
+
+const Bakery = mongoose.model("Bakery", bakerySchema);
+const pretzel = new Bakery({
+  name: "Pretzel",
+  rating: 8,
+  review: "A little expensive",
+});
+const croissant = new Bakery({
+  name: "Croissant",
+  rating: 8,
+  review: "It's all about layers.",
+});
+const cookie = new Bakery({
+  name: "Cookie",
+  rating: 7,
+  review: "Only fall for the soft ones",
 });
 
-const Person = mongoose.model("Person", personSchema);
-const person = new Person({
-  name: "John",
-  age: 37
-});
-person.save();
+Bakery.insertMany([pretzel, croissant, cookie], function(err) {
+  if (err) console.log(err);
+  else console.log("Success!");
+})
