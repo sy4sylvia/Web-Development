0b6d88d67744f4f395f8a8d818de356d02fa7923
Siya Guo <siyaguo@siyas-air.myfiosgateway.com>
2022-04-08 22:57:02 -0400

    custom routes finished

--- todolist-v2-starting-files/app.js
+++ todolist-v2-starting-files/app.js
@@ -19,25 +19,22 @@ mongoose.connect("mongodb://localhost:27017/toDoListDB", {
   useNewUrlParser: true
 });
 //create a new item schema
-const itemsSchema = new mongoose.Schema({
-  name: {
-    type: String
-    // required: [true, "Please check for the name entry"]
-  }
-});
+const itemsSchema = {
+  name: String
+};
 
 //create a new mongoose model -> capitalized
 const Item = mongoose.model("Item", itemsSchema);
 
 //mongoose document
 const item1 = new Item({
-  name: "Buy Food"
+  name: "Chicken with Chestnut"
 });
 const item2 = new Item({
-  name: "Cook Food"
+  name: "Chicken with Taro"
 });
 const item3 = new Item({
-  name: "Eat Food"
+  name: "Chicken with Mushrooms"
 });
 
 const defaultItems = [item1, item2, item3];
@@ -53,6 +50,7 @@ const List = mongoose.model("List", listSchema); //new collection called list>
 
 app.get("/", function(req, res) {
   const day = date.getDate();
+
   Item.find({}, function(err, foundItems) { //find method gives the result of arrays
     if (foundItems.length === 0) { //insert default only if empty
       Item.insertMany(defaultItems, function(err) {
@@ -99,28 +97,25 @@ app.get("/:customListName", function(req, res) {
   });
 });
 
-app.get("/about", function(req, res) {
-  res.render("about");
-});
 
 app.post("/", function(req, res) {
 
   const itemName = req.body.newItem;
   const listName = req.body.list; //corresponds to the name of the button
   //create a new document
-  const curItem = new Item({
+  const item = new Item({
     name: itemName
   });
 
   if (listName != date.getDate()) {
     List.findOne({name: listName}, function (err, foundList){
-      foundList.items.push(curItem);
+      foundList.items.push(item);
       foundList.save();
       res.redirect("/" + listName); //not customListName here
     });
   }
   else {
-    curItem.save(); //mongoose shortcut
+    item.save(); //mongoose shortcut
     res.redirect("/");
   }
 });
@@ -131,7 +126,7 @@ app.post("/delete", function(req, res) {
 
   //decide which route we're on
   if (listName != date.getDate()) {
-    Item.findOneAndUpdate({name: listName}, {$pull: {items: {_id: checkedId}}}, function(err, foundList) {
+    List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: checkedId}}}, function(err, foundList) {
       if (err) console.log(err);
       else {
         console.log("Successfully deleted!");
@@ -144,11 +139,15 @@ app.post("/delete", function(req, res) {
     Item.findByIdAndRemove(checkedId, function(err) {
       if (err) console.log(err);
       else {
-        console.log("Successfully deleted!");
+        console.log("Successfully deleted checked item.");
         res.redirect("/");
       }
     });
   }
+});
+
+app.get("/about", function(req, res) {
+  res.render("about");
 });
 
 
